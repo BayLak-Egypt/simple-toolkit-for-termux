89ae26e7ae4a12e982b11e44aead7536d7f3a67a
BayLak <mh1231231234go@gmail.com>
2026-07-24 02:40:14 +0300

    Update group.js

--- library/group.js
+++ library/group.js
@@ -14,15 +14,21 @@
         "description": "Tools for data encryption, decryption, and hashing",
         "color": "YELLOW"
     },
-       "4": {
-            "title": "Generator Tools",
-        "description": "Miscellaneous and utility scripts",
+    "4": {
+        "title": "Generator Tools",
+        "description": "Scripts and utilities for generating dynamic data, content, and code efficiently.",
+        "color": "YELLOW"
+    },
+    "5": {
+        "title": "Scraper Tools",
+        "description": "Automated scripts and utilities for extracting and collecting data from web sources.",
         "color": "YELLOW"
     }
     ,
-       "5": {
+       "6": {
         "title": "General Tools",
         "description": "Miscellaneous and utility scripts",
-        "color": "YELLOW"
+         "color": "YELLOW"
     }
+    
 }
